9e40aec6f1e4bdac84237edf373b0b7a8bd1b57a
Sara Scaini <sa.scaini@gmail.com>
2020-06-07 22:15:29 +0100

    add errors validations

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,12 +8,26 @@ class ContactForm extends React.Component {
 
     this.state = {
       statusSubmit: '',
-      error:'',
+      submitErr:'',
+      errors: {
+        nameErr: '',
+        messageErr: '',
+        emailErr: ''
+      },
       email: '',
       message: '',
       name: '',
+      isValid: false,
     };
+
+
   }
+
+   // isFormValid = () => {
+   //  const err = Object.values(this.state.errors)
+   //  console.warn(err)
+   // }
+
 
   handleSubmit = (event) => {
     event.preventDefault();
@@ -33,6 +47,7 @@ class ContactForm extends React.Component {
     };
     xhr.send(data);
 
+
     this.setState({
       email: '',
       message: '',
@@ -40,52 +55,91 @@ class ContactForm extends React.Component {
     });
   }
 
+  handleBlur = (event) => {
+    const { value, id } = event.target
+
+    let errors = this.state.errors
+    const regexEmail = RegExp(/\S+@\S+\.\S+/ );
+    switch(id) {
+      case 'name':
+        errors.nameErr = value.length < 3 ? 'Please enter your full name' : ''
+      break;
+      case 'email':
+        errors.emailErr = !regexEmail.test(value) ? 'Please enter a valid email address' : ''
+      break;
+      case 'message':
+        errors.messageErr = value.length < 10 ? 'Your message must be at least 10 characters long' : ''
+      break;
+      default:
+      break;
+    }
+
+    this.setState({ errors});
+  }
+
+  handleFocus = (event) => {
+  const { value, id } = event.target
+
+  this.setState(prevState => ({errors: { ...prevState.errors, nameErr: '' }}))
+
+  }
+
+  handleChange = (event) => {
+    const { value, id } = event.target
+    this.setState({ [id]: value});
+  }
 
   render () {
-
-    const { statusSubmit, name, email, message } = this.state;
-
+    const { statusSubmit, name, email, message, errors } = this.state;
     return (
       <div className="mainFormContainer">
         <form
-          onSubmit={this.handleSubmit}
           action="https://formspree.io/xknqrglb"
           method="POST"
+          onSubmit={this.handleSubmit}
           className="formContainer"
         >
           <div className="inputContainer">
             <label htmlFor="name">Name</label>
             <input
-              onChange={(event) => this.setState({name: event.target.value})}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
               value={name}
               id="name"
               type="text"
               name="name"
               style={{ borderRadius: '4px', padding: '10px'}}
             />
+          <span>{errors.nameErr}</span>
           </div>
 
           <div className="inputContainer">
             <label htmlFor="email">Email</label>
             <input
-              onChange={(event) => this.setState({email: event.target.value})}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
               value={email}
               id="email"
               type="email"
               name="_replyto"
               style={{ borderRadius: '4px', padding: '10px'}}
             />
+          <span>{errors.emailErr}</span>
           </div>
 
           <div className="inputContainer">
             <label htmlFor="message">Message</label>
             <textarea
-              onChange={(event) => this.setState({message: event.target.value})}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
               value={message}
+              id="message"
               name="message"
               rows="10"
               cols="20"
+              maxlength="700"
               style={{ borderRadius: '4px', padding: '10px'}} />
+          <span>{errors.messageErr}</span>
           </div>
 
           <input
@@ -94,6 +148,7 @@ class ContactForm extends React.Component {
             style={{display: "none"}}
            />
           {statusSubmit === "SUCCESS" ? <p>Thanks!</p> : <button className="submitBtn">Submit</button>}
+          <span>{this.state.submitErr}</span>
           {statusSubmit === "ERROR" && <p>Ooops! There was an error.</p>}
 
         </form>
