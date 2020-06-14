3f4efe63aa8ffa18c832aa7b6d08ca90147ada94
Sara Scaini <sa.scaini@gmail.com>
2020-06-14 18:41:16 +0100

    add test background video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
-import ContactForm from "../components/ContactForm"
+// import ContactForm from "../components/ContactForm"
+import video from '../images/video-test.mp4'
 import LogoLarge from '../components/svg/LogoLarge'
-
+import '../styles/index.scss'
 // import SEO from "../components/seo"
 // <SEO title="Home" />
 
@@ -17,15 +18,25 @@ const style = {
 
   return (
     <Layout>
-      <div style={{display: 'flex' }}>
-        <div style={style}>
-          <LogoLarge />
-          <h1>BIM Landscape consultants</h1>
-        </div>
-        <div style={{ background: 'rgba(196, 188, 179, 0.5)', display: 'flex', alignItems: 'center', width: '50%'}}>
-          <div style={{width: '100%', color: '#FFF'}}>
-            <h1>COMING SOON</h1>
-            <h2>Our website is currently under cosntruction.</h2>
+      <div className="mainContainer">
+        <video
+         autoPlay
+         loop                  
+         muted
+        >
+          <source src={video}  type="video/mp4"/>
+        </video>
+        <div className="videoOverlay"></div>    
+        <div className="textContainer" style={style}>
+          <div className="logoContainer">
+            <LogoLarge />
+            <h1>BIM Landscape consultants</h1>
+          </div>
+          <div style={{ background: 'rgba(196, 188, 179, 0.5)', display: 'flex', alignItems: 'center', width: '50%'}}>
+            <div style={{width: '100%', color: '#FFF'}}>
+              <h1>COMING SOON</h1>
+              <h2>Our website is currently under cosntruction.</h2>
+            </div>
           </div>
         </div>
       </div>
