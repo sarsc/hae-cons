14c4ed8c26013e156ca452b870cf6955a14ce33e
Sara Scaini <sa.scaini@gmail.com>
2020-06-14 23:14:08 +0100

    style change

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,6 @@ import '../styles/index.scss'
 
 const IndexPage = () => {
 
-const style = {
-  width: '50%',
-  padding: '40px',
-  textAlign: 'center',
-  margin: '200px 0'
-}
-
   return (
     <Layout>
       <div className="mainContainer">
@@ -27,17 +20,15 @@ const style = {
           <source src={video}  type="video/mp4"/>
         </video>
         <div className="videoOverlay"></div>    
-        <div className="textContainer" style={style}>
+        <div className="textContainer">
           <div className="logoContainer">
             <LogoLarge />
-            <h1>BIM Landscape consultants</h1>
+            <p>BIM Landscape consultants</p>
           </div>
-          <div style={{ background: 'rgba(196, 188, 179, 0.5)', display: 'flex', alignItems: 'center', width: '50%'}}>
-            <div style={{width: '100%', color: '#FFF'}}>
-              <h1>COMING SOON</h1>
+          {/* <div>
+              <h2>COMING SOON</h2>
               <h2>Our website is currently under cosntruction.</h2>
-            </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </Layout>
