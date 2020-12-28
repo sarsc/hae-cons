8c7e03ee58130c7766e649841b3fe14ac2352bcb
Sara Scaini <sa.scaini@gmail.com>
2020-12-28 18:38:19 +0000

    video update

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,25 +2,16 @@ import React, { useRef } from 'react';
 import video from '../assets/forest-bg.mp4';
 import '../styles/video.scss';
 
-const Video = () => {
-  const videoRef = useRef();
-  const playBackSpeed = () => {
-    videoRef.current.playbackRate = 1;
-  };
-
-  return (
-    <div className="videoContainer">
-      <video
-        autoPlay
-        loop
-        muted
-        ref={videoRef}
-        onCanPlay={playBackSpeed}
-      >
-        <source src={video} type="video/mp4" />
-      </video>
-    </div>
-  );
-};
+const Video = () => (
+  <div className="videoContainer">
+    <video
+      autoPlay
+      muted
+      loop
+    >
+      <source src={video} type="video/mp4" />
+    </video>
+  </div>
+);
 
 export default Video;
