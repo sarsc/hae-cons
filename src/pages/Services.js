4c104f224c228bbb4ff6912154b5289b5041080a
Sara Scaini <sa.scaini@gmail.com>
2021-02-04 23:09:11 +0000

    update texts service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -4,62 +4,80 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 import '../styles/services.scss';
 
-const servicesInfo = [
-  {
-    name: 'Modelling',
-    text: ['BIM Level 2 Compliant Landscape Model development using Revit'],
-  },
-  {
-    name: 'Information Management',
-    text: ['Landscape Revit Model Revision', 'Quality Control and Clash Detection Reports'],
-  },
-  {
-    name: 'BIM Strategic Consultancy',
-    text: [
-      'BIM Documents phases: Pre-Contract (EIR, PQQ’s, BEP) Contract (BEP,TIDP, MIDP, MDPT, CIC) Post-Contract (BEP, TIDP, MIDP, MDPT)',
-    ],
-  },
-  {
-    name: 'BIM Implementation',
-    text: [
-      'Workflow Coordination and BIM Strategic Implementation',
-      'Revit Template for Landscape Architecture',
-    ],
-  },
-];
-
-export default () => {
-  const displayServiceText = servicesInfo.map((service, i) => (
-    <div className={`text${i + 1}`}>
-      <div className="gridHeader">
-        <span>
-          0
-          {i + 1}
-        </span>
-        <h4 className="servicesTitles">{service.name}</h4>
+export default () => (
+  <div className="pageContainer servicesContainer" style={{ paddingTop: '0' }}>
+    <div className="leftSideContainer leftText">
+      <div className="headingContainer">
+        <h1 className="heading">Our Services</h1>
+        <h2>BIM Consultancy for Landscape Architecture</h2>
       </div>
-      <div>
-        {service.text.map((text) => (
+    </div>
+    <div className="rightSideContainer rightText">
+      <div className="grid">
+        <div className="text1">
+          <div className="gridHeader">
+            <span>01</span>
+            <h4 className="servicesTitles">Project Modelling</h4>
+          </div>
+          <div>
+            <div className="service">
+              <FontAwesomeIcon icon={faCircle} className="listDots" />
+              <p>BIM Level 2 Compliant</p>
+            </div>
+            <div className="service">
+              <FontAwesomeIcon icon={faCircle} className="listDots" />
+              <p>Landscape Model development using Revitt</p>
+            </div>
+          </div>
+        </div>
+        <div className="text2">
+          <div className="gridHeader">
+            <span>02</span>
+            <h4 className="servicesTitles">Information Management</h4>
+          </div>
+          <div>
+            <div className="service">
+              <FontAwesomeIcon icon={faCircle} className="listDots" />
+              <p>Landscape Revit Model Revision</p>
+            </div>
+            <div className="service">
+              <FontAwesomeIcon icon={faCircle} className="listDots" />
+              <p>Quality Control and Clash Detection Report</p>
+            </div>
+          </div>
+        </div>
+        <div className="text3">
+          <div className="gridHeader">
+            <span>03</span>
+            <h4 className="servicesTitles">BIM Strategic Consultancy</h4>
+          </div>
           <div className="service">
             <FontAwesomeIcon icon={faCircle} className="listDots" />
-            <p>{text}</p>
+            <p>
+              BIM Documents support for all phases:
+              <span style={{ 'padding-top': '20px' }}>Pre-Contract (EIR, PQQ’s, BEP)</span>
+              <span>Contract (BEP,TIDP, MIDP, MDPT, CIC)</span>
+              <span>Post-Contract (BEP, TIDP, MIDP, MDPT)</span>
+            </p>
           </div>
-        ))}
-      </div>
-    </div>
-  ));
-
-  return (
-    <div className="pageContainer servicesContainer" style={{ paddingTop: '0' }}>
-      <div className="leftSideContainer leftText">
-        <div className="headingContainer">
-          <h1 className="heading">our services</h1>
-          <h2>BIM Consultancy for Landscape Architecture</h2>
+        </div>
+        <div className="text4">
+          <div className="gridHeader">
+            <span>04</span>
+            <h4 className="servicesTitles">BIM Implementation</h4>
+          </div>
+          <div>
+            <div className="service">
+              <FontAwesomeIcon icon={faCircle} className="listDots" />
+              <p>BIM Strategy and Coordination Workflow</p>
+            </div>
+            <div className="service">
+              <FontAwesomeIcon icon={faCircle} className="listDots" />
+              <p>Revit Template for Landscape Architecture</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="rightSideContainer rightText">
-        <div className="grid">{displayServiceText}</div>
-      </div>
     </div>
-  );
-};
+  </div>
+);
